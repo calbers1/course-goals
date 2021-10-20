6cf284b7be3802815076b24660d8285d2aa4452f
Connor Albers <calbers1@harding.edu>
2021-10-19 21:49:48 -0500

    initial commit (add conditional formatting/validation)

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = event => {
+    setEnteredValue(event.target.value);
+    setIsValid(true);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    if (enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue);
+    setEnteredValue('');
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input style={{backgroundColor: !isValid ? '#ea6262' : '#fffcfc', border: !isValid ? '1px solid red' : '1px solid black'}} type="text" value={enteredValue} onChange={goalInputChangeHandler} />
+        <span style={{visibility: !isValid ? 'visible' : 'hidden'}}>Please enter a valid goal!</span>
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
