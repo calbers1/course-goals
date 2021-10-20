5aaad73f320a303f87588992ba5ed92668d7d0eb
Connor Albers <calbers1@harding.edu>
2021-10-19 21:52:50 -0500

    prettier code for my sanity

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,33 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
     setIsValid(true);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    if (enteredValue.trim().length === 0){
+    if (enteredValue.trim().length === 0) {
       setIsValid(false);
       return;
     }
     props.onAddGoal(enteredValue);
-    setEnteredValue('');
+    setEnteredValue("");
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input style={{backgroundColor: !isValid ? '#ea6262' : '#fffcfc', border: !isValid ? '1px solid red' : '1px solid black'}} type="text" value={enteredValue} onChange={goalInputChangeHandler} />
-        <span style={{visibility: !isValid ? 'visible' : 'hidden'}}>Please enter a valid goal!</span>
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            backgroundColor: !isValid ? "#ea6262" : "#fffcfc",
+            border: !isValid ? "1px solid red" : "1px solid black",
+          }}
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
+        <span style={{ visibility: !isValid ? "visible" : "hidden" }}>
+          Please enter a valid goal!
+        </span>
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
